4037dee280ab124ca8189a589d3ed942c468b9f4
Zumbahlen <ejzumba@ilstu.edu>
2019-02-06 11:09:15 -0600

    fixed @value issue

--- src/main/java/com/seniorproject/twitterMicroservice/rest/Twittercontroller.java
+++ src/main/java/com/seniorproject/twitterMicroservice/rest/Twittercontroller.java
@@ -6,8 +6,10 @@ package com.seniorproject.twitterMicroservice.rest;
 import java.util.List;
 
 import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
@@ -23,34 +25,43 @@ import org.springframework.web.client.RestTemplate;
  * @author ericz
  *
  */
-@Component
-@PropertySource("classpath:application.properties")
+//@Component
+//@PropertySource("classpath:application.properties")
 @RestController
 @RequestMapping("api/v1")
 public class Twittercontroller {
 	private TwitterServiceProvider tsp;
 	private Twitter api;
 	private final Logger log = Logger.getLogger(Twittercontroller.class);
+	private String key;
+	private String secret;
 	
-	@Value("${api.key}")
-	String key;
+//	@Autowired
+//	private Environment env;
 	
-	@Value("${api.secret}")
-	String secret;
 	
-	public Twittercontroller(){
+	
+	@Autowired
+	public Twittercontroller(@Value("${api.key}") String key1, @Value("${api.secret}") String secret1){
+		
+		this.key= key1;
+		this.secret= secret1;		
+		
 		tsp= new TwitterServiceProvider(key, secret);
 		api= tsp.getApi("4853842084-TIrvpmvCFPQiSlWv2PGVHiPFO6VuBor5eq3Czki","oQMtztRty6feDwQW75e9yEo0e9v42Y84pBUKn0rwFgA4u");
+		
 	}
 
 	@RequestMapping(value = "/search/{hashtag}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public List<Tweet> searchTweet(@PathVariable final String hashtag){
+		
 		return api.searchOperations().search(hashtag, 20).getTweets();
 	}
 	
 	@GetMapping("/test")
 	public List<Tweet> request() {
 		List<Tweet> retVal;
+		
 		retVal= api.timelineOperations().getHomeTimeline();
 		
 		
